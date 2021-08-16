3da4b23f88fd85a0cbecef8233f74c1619fe03b6
EmilRegis <a730ut@gmail.com>
2021-08-16 15:14:07 +0600

    add vue metrika for yabdex

--- src/main.js
+++ src/main.js
@@ -6,10 +6,15 @@ import VueSmoothScroll from 'vue3-smooth-scroll';
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueCarousel from 'vue-carousel';
-
-
+import VueYandexMetrika from 'vue-yandex-metrika';
+const router = window.location.href;
 
 createApp(App).use(store).use(VueAxios, axios).use(VueCarousel).use(VueSmoothScroll, {
     duration: 1000,
     updateHistory: false
+}).use(VueYandexMetrika, {
+    id: 84032422,
+    router: router,
+    env: process.env.NODE_ENV
+    // other options
 }).mount('#app');
